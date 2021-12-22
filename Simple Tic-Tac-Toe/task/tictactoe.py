ae6a27972248f78495af7d17af62f958a282bf09
Korwin <korwin-bieniek@wp.pl>
2021-12-22 21:48:03 +0100

    Stage 2/5, choose which cells should be allocated with signs.

--- Simple Tic-Tac-Toe/task/tictactoe.py
+++ Simple Tic-Tac-Toe/task/tictactoe.py
@@ -1,3 +1,13 @@
-print('''X O X
-O X O
-X X O''')
+def create_board():
+    cells = input()
+    print('---------')
+    for i in range(1, 10):
+        if i == 1 or i == 4 or i == 7:
+            print('| ', end='')
+        print(cells[i - 1], end=' ')
+        if i % 3 == 0:
+            print('|')
+    print('---------')
+
+
+create_board()
